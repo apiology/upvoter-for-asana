2e70be3b3c6e6dac1d085c3b331b4e063fff2bd7
Vince Broz <vince@broz.cc>
2021-01-21 11:00:04 -0500

    Remove debugging code

--- background.js
+++ background.js
@@ -6,7 +6,6 @@
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
-    // console.log('asanaAccessToken: ' + asanaAccessToken);
     console.log('inputChanged: ' + text);
     suggest([
       {content: text + " one", description: "the first one"},
