adde5fd13a803a35d141d0d53a6cdfbea7bb4d6e
Vince Broz <vince@broz.cc>
2021-01-21 10:55:59 -0500

    Add node-asana library for Asana access

--- background.js
+++ background.js
@@ -1,8 +1,29 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
+// https://github.com/GoogleChrome/chrome-extensions-samples/blob/1d8d137d20fad5972292377dc22498529d2a4039/api/omnibox/simple-example/background.js
+
+'use strict';
+
+// This event is fired each time the user updates the text in the omnibox,
+// as long as the extension's keyword mode is still active.
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    // console.log('asanaAccessToken: ' + asanaAccessToken);
+    console.log('inputChanged: ' + text);
+    console.log('Asana: ' + Asana);
+    const client = Asana.Client.create().useAccessToken(asanaAccessToken);
+    client.users.me().then(function(me) { console.log(me); });
+    // console.log('Asana.Client: ' + Asana.Client);
+//    console.log('asana: ' + asana);
+//    console.log('Client: ' + Client);
+    suggest([
+      {content: text + " one", description: "the first one"},
+      {content: text + " number two", description: "the second entry"}
+    ]);
+  });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    console.log('inputEntered: ' + text);
+    alert('You just typed "' + text + '"');
   });
 });
