ec97dd02205602657d95add63cf30f92b069efa1
Vince Broz <vince@broz.cc>
2021-02-16 18:54:45 -0500

    Only need one Python version

--- fix.sh
+++ fix.sh
@@ -123,7 +123,7 @@ ensure_pyenv() {
 ensure_python_versions() {
   # You can find out which feature versions are still supported / have
   # been release here: https://www.python.org/downloads/
-  python_versions="$(latest_python_version 3.9) $(latest_python_version 3.8) $(latest_python_version 3.7) $(latest_python_version 3.6)"
+  python_versions="$(latest_python_version 3.9)"
 
   echo "Latest Python versions: ${python_versions}"
 
