6323d2c0259e7890e3cf76396c8e46c3b0038ac6
Vince Broz <vince@broz.cc>
2021-10-01 09:57:51 -0400

    Set up upvoter_for_asana to add dummy buttons to dependent task dialog

--- src/content-script.js
+++ src/content-script.js
@@ -1,3 +1,21 @@
 setInterval(() => {
-  console.log('foo');
+  const bodyNodesClassName = 'CompleteTaskWithIncompletePrecedentTasksConfirmationModal-bodyNode';
+
+  const bodyNodes = Array.from(document.getElementsByClassName(bodyNodesClassName));
+  for (const bodyNode of bodyNodes) {
+    const linkClassName = 'CompleteTaskWithIncompletePrecedentTasksConfirmationModal-primaryNavigationLink';
+    const links = Array.from(bodyNode.getElementsByClassName(linkClassName));
+    const upvoteLinkClassName = 'upvoter-upvote-link';
+    const upvoteLinks = Array.from(bodyNode.getElementsByClassName(upvoteLinkClassName));
+    if (upvoteLinks.length === 0) {
+      for (const link of links) {
+        const newNode = document.createElement('a');
+        newNode.classList.add(upvoteLinkClassName);
+        newNode.innerHTML = '<div style="font-size:20px;display:inline">⬆️</div>[123] ';
+        const linkParent = link.parentNode;
+        linkParent.insertBefore(newNode, link);
+        console.log('link', link);
+      }
+    }
+  }
 }, 1000);
