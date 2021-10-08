381fc1f63ae04b051577aae25044536ad2d18e37
Vince Broz <vince@broz.cc>
2021-10-08 09:21:58 -0400

    Break content script into functions

--- src/content-script.js
+++ src/content-script.js
@@ -2,6 +2,27 @@ const {
   upvoteTask, client, logSuccess,
 } = require('./upvoter.js');
 
+const onDependentTaskClickFn = (dependentTaskGid, link) => (event) => {
+  client.tasks.getTask(dependentTaskGid)
+    .then(upvoteTask)
+    .then(logSuccess)
+    .then(() => {
+      link.innerHTML += ' [^]';
+    });
+  event.stopPropagation();
+};
+
+const upvoteLinkClassName = 'upvoter-upvote-link';
+
+const fixUpLinkToDependency = (link) => {
+  const url = link.getAttribute('href');
+  const dependentTaskGid = url.split('/').at(-1);
+  link.removeAttribute('href');
+  link.innerHTML += ' [...]';
+  link.onclick = onDependentTaskClickFn(dependentTaskGid, link);
+  link.classList.add(upvoteLinkClassName);
+};
+
 setInterval(() => {
   const bodyNodesClassName = 'CompleteTaskWithIncompletePrecedentTasksConfirmationModal-bodyNode';
 
@@ -9,27 +30,11 @@ setInterval(() => {
   for (const bodyNode of bodyNodes) {
     const linkClassName = 'CompleteTaskWithIncompletePrecedentTasksConfirmationModal-primaryNavigationLink';
     const links = Array.from(bodyNode.getElementsByClassName(linkClassName));
-    const upvoteLinkClassName = 'upvoter-upvote-link';
     const upvoteLinks = Array.from(bodyNode.getElementsByClassName(upvoteLinkClassName));
     if (upvoteLinks.length === 0) {
+      // don't process links twice; if we've marked one as processed, consider this done.
       for (const link of links) {
-        console.log('link', link);
-        console.log('innerHTML', link.innerHTML);
-        const url = link.getAttribute('href');
-        const dependentTaskGid = url.split('/').at(-1);
-        link.removeAttribute('href');
-        console.log('Looking for dependentTaskGid', dependentTaskGid);
-        link.innerHTML += ' [...]';
-        link.onclick = (event) => {
-          client.tasks.getTask(dependentTaskGid)
-            .then(upvoteTask)
-            .then(logSuccess)
-            .then(() => {
-              link.innerHTML += ' [^]';
-            });
-          event.stopPropagation();
-        };
-        link.classList.add(upvoteLinkClassName);
+        fixUpLinkToDependency(link);
       }
     }
   }
