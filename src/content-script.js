e637187fef73fbd9fdcf36cb6b42581097287795
Vince Broz <vince@broz.cc>
2021-10-19 09:11:24 -0400

    Add prototype code for upvoterKeyDown()

--- src/content-script.js
+++ src/content-script.js
@@ -42,18 +42,33 @@ function* dependencyLinks() {
   for (const bodyNode of bodyNodes) {
     const linkClassName = 'CompleteTaskWithIncompletePrecedentTasksConfirmationModal-primaryNavigationLink';
     const links = Array.from(bodyNode.getElementsByClassName(linkClassName));
-    const upvoteLinks = Array.from(bodyNode.getElementsByClassName(upvoteLinkClassName));
-    if (upvoteLinks.length === 0) {
-      // don't process links twice; if we've marked one as processed, consider this done.
-      for (const link of links) {
-        yield link;
-      }
+    for (const link of links) {
+      yield link;
     }
   }
 }
 
 setInterval(() => {
   for (const dependencyLink of dependencyLinks()) {
+    // don't process links twice; if we've marked one as processed, consider this done.
+    if (dependencyLink.classList.contains(upvoteLinkClassName)) {
+      break;
+    }
     fixUpLinkToDependency(dependencyLink);
   }
 }, 1000);
+
+console.log('Defining upvoterKeyDown');
+function upvoterKeyDown(e) {
+  // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+  const nonZeroDigits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  if (e.metaKey && e.ctrlKey && nonZeroDigits.includes(e.key)) {
+    const num = parseInt(e.key, 10);
+    const links = Array.from(dependencyLinks());
+    const linkFound = links[num - 1];
+    console.log('linkFound', linkFound);
+  }
+}
+
+document.addEventListener('keydown', upvoterKeyDown, false);
+console.log('Registered keydown listener', upvoterKeyDown);
