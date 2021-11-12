3be6ceed5f5c14e4b86c05ba48493b2a0b9e77c8
Vince Broz <vince@broz.cc>
2021-11-12 13:42:23 -0500

    Revert to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,20 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      // asana library uses the node API and expects users to use
-      // webpack to polyfill it when using BrowserJS
-      fs: false, // not particularly used by asana
-      url: require.resolve('url/'),
-      util: require.resolve('util/'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer/'),
-    },
-  },
   output: {
     filename: '[name].js',
+  },
+  node: {
+    // https://github.com/webpack-contrib/css-loader/issues/447
+    //
+    // ERROR in ./node_modules/asana/lib/auth/index.js
+    // Module not found: Error: Can't resolve './auto_detect' in
+    //   '/Users/broz/src/upvoter_for_asana/node_modules/asana/lib/auth'
+    // @ ./node_modules/asana/lib/auth/index.js 6:21-45
+    // @ ./node_modules/asana/index.js
+    // @ ./src/upvoter.js
+    // @ multi ./src/background.js ./src/upvoter.js
+    fs: 'empty',
   },
   // https://stackoverflow.com/questions/48047150/chrome-extension-compiled-by-webpack-throws-unsafe-eval-error
   devtool: 'cheap-module-source-map',
