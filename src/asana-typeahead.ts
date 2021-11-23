d36a0437a2ced44792bdf7e866496be58e5b9baa
Vince Broz <vince@broz.cc>
2021-11-23 08:40:38 -0500

    DRY up Gid constant fetches

--- src/asana-typeahead.ts
+++ src/asana-typeahead.ts
@@ -12,25 +12,45 @@ import { escapeHTML } from './omnibox';
 
 export const client = Asana.Client.create().useAccessToken(asanaAccessToken);
 
-export const findAndSaveWorkspaceGid = (
-  workspacesResult: Asana.resources.ResourceList<Asana.resources.Workspaces.Type>
-) => new Promise<string | null>((resolve, reject) => {
-  // https://stackoverflow.com/questions/44013020/using-promises-with-streams-in-node-js
-  const stream = workspacesResult.stream();
-  stream.on('data', (workspace: Asana.resources.Workspaces.Type) => {
-    if (workspace.name === workspaceName) {
-      console.log(`Found workspace GID as ${workspace.gid}`);
-      resolve(workspace.gid);
-    }
+export function findGid<T extends Asana.resources.Resource>(
+  resourceList: Asana.resources.ResourceList<T>,
+  isCorrectResource: (resource: T) => boolean
+) {
+  return new Promise<string | null>((resolve, reject) => {
+    // If I had esnext.asynciterable in
+    // tsconfig.json#compilerOptions.lib, and if node-asana's
+    // BufferedReadableasy supported async iterators, I could do:
+    //
+    // for await (const customField of customFieldsResult.stream()) {
+    //   if (saveCustomFieldGidIfRightName(customField)) {
+    //     return;
+    //   }
+    // }
+    //
+    // as-is, that gives me this error from tsc: Type
+    // 'ResourceStream<Type>' must have a '[Symbol.asyncIterator]()'
+    // method that returnsan async
+    // iterator. [2504]
+    //
+    // https://github.com/Asana/node-asana/blob/8db5f44ff9acb8df04317c5c4db0ac4a300ba8b0/lib/util/buffered_readable.js
+    // https://stackoverflow.com/questions/44013020/using-promises-with-streams-in-node-js
+    // https://stackoverflow.com/questions/47219503/how-do-async-iterators-work-error-ts2504-type-must-have-a-symbol-asynciterat
+    const stream = resourceList.stream();
+    stream.on('data', (resource: T): void => {
+      if (isCorrectResource(resource)) {
+        console.log(`Found ${resource.gid}`);
+        resolve(resource.gid);
+      }
+    });
+    stream.on('end', () => resolve(null));
+    stream.on('finish', () => resolve(null));
+    stream.on('error', () => reject());
   });
-  stream.on('end', () => resolve(null));
-  stream.on('finish', () => resolve(null));
-  stream.on('error', () => reject());
-});
+}
 
 export const workspaceGidFetch: Promise<string> = (async () => {
   const workspaces = await client.workspaces.getWorkspaces();
-  const workspaceGid = await findAndSaveWorkspaceGid(workspaces);
+  const workspaceGid = await findGid(workspaces, (workspace) => workspace.name === workspaceName);
   if (workspaceGid == null) {
     logError('Could not find workspace GID!');
   }
@@ -69,7 +89,5 @@ export const pullResult = async (text: string) => {
   });
 
   // https://developers.asana.com/docs/typeahead
-  const typeaheadResult = await client.typeahead.typeaheadForWorkspace(workspaceGid, query);
-
-  return typeaheadResult;
+  return client.typeahead.typeaheadForWorkspace(workspaceGid, query);
 };
