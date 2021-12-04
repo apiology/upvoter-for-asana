647a38e8d513ad29f0bbd52c2549cdd81909010d
Vince Broz <vince@broz.cc>
2021-12-04 10:04:17 -0500

    Move to webpack 5 (#90)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+// webpack requires a 'require' here, which seems reasonable as it's,
+// you know, the thing that provides import to begin with:
+//
+// SyntaxError: Cannot use import statement outside a module
+const webpack = require('webpack'); // eslint-disable-line @typescript-eslint/no-var-requires
+
 module.exports = {
   entry: {
     background: ['./src/background.ts', './src/upvoter.ts'],
@@ -19,24 +25,45 @@ module.exports = {
   // https://stackoverflow.com/questions/43595555/webpack-cant-resolve-typescript-modules
   resolve: {
     extensions: ['.ts', '.js', '.json'],
+    fallback: {
+      // The node-asana library uses the node API and expects users to
+      // use webpack to polyfill it when using BrowserJS:
+      //
+      // https://webpack.js.org/configuration/resolve/
+      fs: false, // not particularly used by asana
+      url: require.resolve('url'),
+      util: require.resolve('util'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+      // see the plugins key below for more complex handling of
+      // process.
+    },
   },
   mode: 'development', // override with webpack --mode=production on CLI builds
   output: {
     filename: '[name].js',
   },
-  node: {
-    // https://github.com/webpack-contrib/css-loader/issues/447
-    //
-    // ERROR in ./node_modules/asana/lib/auth/index.js
-    // Module not found: Error: Can't resolve './auto_detect' in
-    //   '/Users/broz/src/upvoter_for_asana/node_modules/asana/lib/auth'
-    // @ ./node_modules/asana/lib/auth/index.js 6:21-45
-    // @ ./node_modules/asana/index.js
-    // @ ./src/upvoter.js
-    // @ multi ./src/background.js ./src/upvoter.js
-    fs: 'empty',
-  },
   // 'inline-source-map' is suggested by https://webpack.js.org/guides/typescript/
   // 'cheap-module-source-map' is suggested by https://stackoverflow.com/questions/48047150/chrome-extension-compiled-by-webpack-throws-unsafe-eval-error
   devtool: 'cheap-module-source-map',
+  plugins: [
+    // node-asana uses process.nextTick, which this provides:
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    // The node-util polyfill looks up 'process.env.NODE_DEBUG' to
+    // toggle debugging without checking for process.env existing.
+    //
+    // The node-process polyfill doesn't provide process.env at all.
+    //
+    // There's no note of this on the webpack page recommending
+    // node polyfills.
+    //
+    // https://github.com/browserify/node-util/issues/43
+    new webpack.DefinePlugin({
+      process: {
+        env: '{}',
+      },
+    }),
+  ],
 };
