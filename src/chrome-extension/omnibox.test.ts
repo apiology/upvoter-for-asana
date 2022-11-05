565fbad6f485d7e2f283b34281df2f6c26af8d30
Vince Broz <vince@broz.cc>
2022-11-04 22:00:25 -0400

    Add another test case

--- src/chrome-extension/omnibox.test.ts
+++ src/chrome-extension/omnibox.test.ts
@@ -1,6 +1,6 @@
 import Asana from 'asana';
 import { omniboxInputEnteredListener } from './omnibox.js';
-import { actOnInputData, logSuccess } from '../upvoter-for-asana.js';
+import { actOnInputData, logSuccess, pullSuggestions } from '../upvoter-for-asana.js';
 
 jest.mock('../upvoter-for-asana');
 
@@ -36,5 +36,18 @@ test('omniboxInputEnteredListenerExceptionWithAlertAvailable', async () => {
 
   await expect(omniboxInputEnteredListener('upvoter-for-asana:mumble')).rejects.toMatch('123');
   expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:mumble');
+  expect(mockLogSuccess).not.toHaveBeenCalled();
+});
+
+test('omniboxInputEnteredListenerDefaultEmptyList', async () => {
+  const mockActOnInputData = jest.mocked(actOnInputData);
+  const mockLogSuccess = jest.mocked(logSuccess);
+  const mockPullSuggestions = jest.mocked(pullSuggestions);
+  const task = {} as Asana.resources.Tasks.Type;
+  mockActOnInputData.mockResolvedValue(task);
+  mockPullSuggestions.mockResolvedValue([]);
+
+  await expect(omniboxInputEnteredListener('blah')).rejects.toEqual(new Error('No results for "blah"'));
+  expect(mockActOnInputData).not.toHaveBeenCalled();
   expect(mockLogSuccess).not.toHaveBeenCalled();
 });
