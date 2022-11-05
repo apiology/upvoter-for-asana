7662ec3df8e1f2988f1c71f0cee4bb3d90df58db
Vince Broz <vince@broz.cc>
2022-11-04 22:08:40 -0400

    Test another case

--- src/chrome-extension/omnibox.test.ts
+++ src/chrome-extension/omnibox.test.ts
@@ -1,6 +1,8 @@
 import Asana from 'asana';
 import { omniboxInputEnteredListener } from './omnibox.js';
-import { actOnInputData, logSuccess, pullSuggestions } from '../upvoter-for-asana.js';
+import {
+  actOnInputData, logSuccess, pullSuggestions, Suggestion,
+} from '../upvoter-for-asana.js';
 
 jest.mock('../upvoter-for-asana');
 
@@ -50,4 +52,20 @@ test('omniboxInputEnteredListenerDefaultEmptyList', async () => {
   await expect(omniboxInputEnteredListener('blah')).rejects.toEqual(new Error('No results for "blah"'));
   expect(mockActOnInputData).not.toHaveBeenCalled();
   expect(mockLogSuccess).not.toHaveBeenCalled();
+});
+
+test('omniboxInputEnteredListenerDefaultMultipleItems', async () => {
+  const mockActOnInputData = jest.mocked(actOnInputData);
+  const mockLogSuccess = jest.mocked(logSuccess);
+  const mockPullSuggestions = jest.mocked(pullSuggestions);
+  const task = {} as Asana.resources.Tasks.Type;
+  mockActOnInputData.mockResolvedValue(task);
+  const item1: Suggestion = { url: 'upvoter-for-asana:foo', description: 'dfoo' };
+  const item2: Suggestion = { url: 'upvoter-for-asana:bar', description: 'dbar' };
+  const item3: Suggestion = { url: 'upvoter-for-asana:baz', description: 'dbaz' };
+  mockPullSuggestions.mockResolvedValue([item1, item2, item3]);
+
+  await omniboxInputEnteredListener('foo');
+  expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:foo');
+  expect(mockLogSuccess).toHaveBeenCalledWith(task);
 });
