3d71fd760fb7146339ea48e0b3020aa85f1dd793
Vince Broz <vince@broz.cc>
2024-10-10 07:18:28 -0400

    Update template

--- src/chrome-extension/chrome-extension-logger.test.ts
+++ src/chrome-extension/chrome-extension-logger.test.ts
@@ -1,21 +1,38 @@
+import 'jest-chrome';
 import { ChromeExtensionLogger } from './chrome-extension-logger.js';
 
 test('create class', () => {
   expect(new ChromeExtensionLogger()).not.toBeNull();
 });
 
-// test('log', async () => {
-//   expect(new ChromeExtensionLogger().log).toBe(console.log);
-// });
+test('log', async () => {
+  const spy = jest.spyOn(console, 'log').mockImplementation(() => {
+    // just used to verify it's called
+  });
+  new ChromeExtensionLogger().log('test');
+  expect(spy).toHaveBeenCalledWith('Upvoter for Asana', 'test');
+});
 
-// test('debug', async () => {
-//   expect(new ChromeExtensionLogger().debug).toBe(console.debug);
-// });
+test('debug', async () => {
+  const spy = jest.spyOn(console, 'debug').mockImplementation(() => {
+    // just used to verify it's called
+  });
+  new ChromeExtensionLogger().debug('test');
+  expect(spy).toHaveBeenCalledWith('Upvoter for Asana', 'test');
+});
 
-// test('warn', async () => {
-//   expect(new ChromeExtensionLogger().warn).toBe(console.warn);
-// });
+test('warn', async () => {
+  const spy = jest.spyOn(console, 'warn').mockImplementation(() => {
+    // just used to verify it's called
+  });
+  new ChromeExtensionLogger().warn('test');
+  expect(spy).toHaveBeenCalledWith('Upvoter for Asana', 'test');
+});
 
-// test('error', async () => {
-//   expect(new ChromeExtensionLogger().error).toBe(console.error);
-// });
+test('error', async () => {
+  const spy = jest.spyOn(console, 'error').mockImplementation(() => {
+    // just used to verify it's called
+  });
+  new ChromeExtensionLogger().error('test');
+  expect(spy).toHaveBeenCalledWith('Upvoter for Asana', 'test');
+});
